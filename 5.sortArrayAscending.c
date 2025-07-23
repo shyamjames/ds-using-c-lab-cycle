d0b6608f565902bafdc9ae08e017099b2029f184
Shyam James <shyamjames74@gmail.com>
2025-07-23 15:20:53 +0530

    sort array ascending

---
+++ 5.sortArrayAscending.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int a[5];
+
+void read(){
+    printf("Enter the elements of the array:\n");
+    int i;
+    for(i=0;i<5;i++){
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
+}
+
+void disp(){
+    int i;
+    for(i=0;i<5;i++){
+        printf("%d\t",a[i]);
+    }
+}
+
+void arraySort(){
+    int temp,i,j;
+    for (i = 0; i < 5; ++i) {
+    for (j = i + 1; j < 5; ++j) {
+        if (a[i] > a[j]) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
+printf("\nSorted Array:\n");
+disp();
+}
+
+int main(){
+    read();
+    printf("Unsorted Array:\n");
+    disp();
+    arraySort();
+
+    return 0;
+}
